17815f6a1f2d5a2135f623b70f89ef2aecabfb84
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-23 02:37:43 +0700

    menambahkan borderradius, boxshadow dna alignment

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app_081/constants.dart';
 
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
@@ -16,7 +17,19 @@ class MyWidget extends StatelessWidget {
             height: size.height *0.8,
             width: size.width*0.75,
             decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(63),
+                bottomLeft: Radius.circular(63)
+              ),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 10),
+                  blurRadius: 60,
+                  color: kPrimaryColor.withAlpha((255*0.29).round())
+                )
+              ],
               image: DecorationImage(
+                alignment: Alignment.centerLeft,
                 fit: BoxFit.cover,
                 image: AssetImage("assets/images/img.png"))
             ),
