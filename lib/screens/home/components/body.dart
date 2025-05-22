f1f9f1bca3df7fc7913c6e05893f0641302a67dc
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-22 22:50:28 +0700

    menambahkan mediaquery untuk size

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -34,6 +34,7 @@ class RecommendPlantCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Container(
       width: size.width * 0.4,
       child: Column(
