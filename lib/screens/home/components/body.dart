5654fbcf78798135a4e0164c25777c5218f2eca4
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-22 19:38:16 +0700

    mengekstrak widget TitleWithCustomUnderline dan menambahkan key dan parameter text

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -12,29 +12,43 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Headwithsearch(size: size),
-          Container(
-            height: 24,
-            child: Stack(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-                  child: Text(
-                    "Recommended",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                    margin: EdgeInsets.only(right: kDefaultPadding / 4),
-                    height: 7,
-                    color: kPrimaryColor.withAlpha((255*0.2).round()),
-                  ))
-              ],
+          TitleWithCustomUnderline(text: "Recommended",)
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({
+    Key? key,
+    required this.text,
+  }): super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-          )
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withAlpha((255*0.2).round()),
+            ))
         ],
       ),
     );
