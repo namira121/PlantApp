9a452adf8f3df72570893ce3d05912b1de7d4084
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-22 23:50:20 +0700

    memindahkan featured plants ke file yang berbeda

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:plant_app_081/constants.dart';
 import 'title_with_more_btn.dart';
 import 'headwithsearch.dart';
+import 'featured_plants.dart';
 import 'recommend_plants.dart';
 
 class Body extends StatelessWidget {
@@ -22,60 +23,15 @@ class Body extends StatelessWidget {
           TitleWithMoreButton(
             title: "Featured Plants", 
             press: (){}),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: <Widget>[
-                FeaturedPlantCard(
-                  image: "assets/images/bottom_img_1.png", 
-                  press: (){},
-                ),
-                FeaturedPlantCard(
-                  image: "assets/images/bottom_img_2.png", 
-                  press: (){},
-                ),
-              ],
-            ),
-          )
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding)
         ],
       ),
     );
   }
 }
 
-class FeaturedPlantCard extends StatelessWidget {
-  const FeaturedPlantCard({
-    super.key,
-    required this.image, required this.press,
-  });
 
-  final String image;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-        margin: EdgeInsets.only(
-          left: kDefaultPadding,
-          top: kDefaultPadding / 2,
-          bottom: kDefaultPadding / 2
-        ),
-        width: size.width * 0.8,
-        height: 185,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage(image),
-          )
-        ),
-      ),
-    );
-  }
-}
 
 
 
