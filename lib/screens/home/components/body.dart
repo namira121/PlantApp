b0f2b80d886a9fea40982cba0ca042a9fcb24e98
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-22 18:01:27 +0700

    membuat bagian untuk body menggunakan column

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: <Widget>[
+        Container()
+      ],
+    );
+  }
+}
