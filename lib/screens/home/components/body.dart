a766ee6bb463516668b7a3b51c5936f63f49dda5
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-22 22:58:26 +0700

    menambahkan parameter image, title, country, press, dan price untuk RecommendPlantCard

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -17,7 +17,13 @@ class Body extends StatelessWidget {
             title: "Recommended",
             press: (){},
           ),
-          RecommendPlantCard(size: size)
+          RecommendPlantCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            press: (){},
+            price: 440,
+          )
         ],
       ),
     );
@@ -26,11 +32,17 @@ class Body extends StatelessWidget {
 
 class RecommendPlantCard extends StatelessWidget {
   const RecommendPlantCard({
-    super.key,
-    required this.size,
+    super.key, 
+    required this.image, 
+    required this.title, 
+    required this.country, 
+    required this.price, 
+    required this.press,
   });
 
-  final Size size;
+  final String image, title, country;
+  final int price;
+  final VoidCallback press;
 
   @override
   Widget build(BuildContext context) {
@@ -39,47 +51,50 @@ class RecommendPlantCard extends StatelessWidget {
       width: size.width * 0.4,
       child: Column(
         children: <Widget>[
-          Image.asset("assets/images/image_1.png"),
-          Container(
-            padding: EdgeInsets.all(kDefaultPadding/2),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10)
-              ),
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, 10),
-                  blurRadius: 50,
-                  color: kPrimaryColor.withAlpha((255 * 0.23).round()),
-                )
-              ]
-            ),
-            child: Row(
-              children: <Widget>[
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "Samantha\n".toUpperCase(),
-                        style: Theme.of(context).textTheme.labelLarge,
-                      ),
-                      TextSpan(
-                        text: "Russia".toUpperCase(),
-                        style: TextStyle(
-                          color: kPrimaryColor.withAlpha((255*0.5).round())
-                        )
-                      )
-                    ]
-                  )
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
+            child: Container(
+              padding: EdgeInsets.all(kDefaultPadding/2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10)
                 ),
-                Text('\$440', 
-                style: Theme.of(context)
-                .textTheme
-                .labelLarge!
-                .copyWith(color: kPrimaryColor))
-              ],
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withAlpha((255 * 0.23).round()),
+                  )
+                ]
+              ),
+              child: Row(
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "$title\n".toUpperCase(),
+                          style: Theme.of(context).textTheme.labelLarge,
+                        ),
+                        TextSpan(
+                          text: "$country".toUpperCase(),
+                          style: TextStyle(
+                            color: kPrimaryColor.withAlpha((255*0.5).round())
+                          )
+                        )
+                      ]
+                    )
+                  ),
+                  Text('\$$price', 
+                  style: Theme.of(context)
+                  .textTheme
+                  .labelLarge!
+                  .copyWith(color: kPrimaryColor))
+                ],
+              ),
             ),
           )
         ],
