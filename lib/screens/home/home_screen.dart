f2e77f1739a425992b3e29d2f76baca15e0ae644
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-23 01:36:19 +0700

    menambahkan container untuk bottomnavigationbar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:plant_app_081/constants.dart';
 import 'package:plant_app_081/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,6 +11,19 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(),
       body: Body(),
+      bottomNavigationBar: Container(
+        height: 80,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, -10),
+              blurRadius: 35,
+              color: kPrimaryColor.withAlpha((255*0.38).round())
+            )
+          ]
+        ),
+      ),
     );
   }
 
