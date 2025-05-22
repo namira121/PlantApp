5bcf5df97ea3c5f6c01ce3a53544c8ea842d1d81
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-23 02:34:12 +0700

    menambahkan container untuk gambar

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -13,7 +13,13 @@ class MyWidget extends StatelessWidget {
             child: Column()
           ),
           Container(
-            
+            height: size.height *0.8,
+            width: size.width*0.75,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/images/img.png"))
+            ),
           )
         ],
         )
