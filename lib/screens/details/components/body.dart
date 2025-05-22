8f74e11e7e2d3f529af0a417a19d77063e23a52c
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-23 03:37:02 +0700

    manambahkan title, country dan price sebagai detail produk. Menambahkan button untuk buy now

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:plant_app_081/constants.dart';
 import 'package:plant_app_081/screens/details/components/icon_card.dart';
 import 'package:plant_app_081/screens/details/components/image_and_icon.dart';
+import 'package:plant_app_081/screens/details/components/title_and_price.dart';
 
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
@@ -13,12 +14,41 @@ class MyWidget extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          ImageAndIcon(size: size)
+          ImageAndIcon(size: size),
+          TitleAndPrice(
+            title: "Angelica",
+            country: "Russia",
+            price: 440,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20)
+                      )
+                    )
+                  ),
+                  onPressed: (){}, 
+                  child: Text("Buy Now",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16
+                  ),)),
+              )
+            ],
+          )
         ],
       ),
     );
   }
 }
+
+
 
 
 
