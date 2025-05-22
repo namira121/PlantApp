559cb9d4c766b12d8174da9a64ca8518956e4ff3
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-23 02:51:00 +0700

    membuat container untuk sidebar halaman detail

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -10,42 +10,75 @@ class MyWidget extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Column(
       children: [
-        Row(children: [
-          Expanded(
-            child: Column(
-              children: [
-                IconButton(
-                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  onPressed: (){
-                    Navigator.pop(context);
-                  }, 
-                  icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
-                  )
-              ],
-            )
-          ),
-          Container(
-            height: size.height *0.8,
-            width: size.width*0.75,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(63),
-                bottomLeft: Radius.circular(63)
-              ),
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, 10),
-                  blurRadius: 60,
-                  color: kPrimaryColor.withAlpha((255*0.29).round())
-                )
-              ],
-              image: DecorationImage(
-                alignment: Alignment.centerLeft,
-                fit: BoxFit.cover,
-                image: AssetImage("assets/images/img.png"))
+        SizedBox(
+          height: size.height*0.8,
+          child: Row(
+            children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: kDefaultPadding*3),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                        onPressed: (){
+                          Navigator.pop(context);
+                        }, 
+                        icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
+                        ),
+                    ),
+                    Spacer(),
+                    Container(
+                      padding: EdgeInsets.all(kDefaultPadding/2),
+                      height: 62,
+                      width: 62,
+                      decoration: BoxDecoration(
+                        color: kBackgroundColor,
+                        borderRadius: BorderRadius.circular(6),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 15),
+                            blurRadius: 22,
+                            color: kPrimaryColor.withAlpha((255*0.22).round())
+                          ),
+                          BoxShadow(
+                            offset: Offset(-15, -15),
+                            blurRadius: 20,
+                            color: Colors.white
+                          )
+                        ]
+                      ),
+                      child: SvgPicture.asset("assets/icons/suns.svg"),
+                    )
+                  ],
+                ),
+              )
             ),
-          )
-        ],
+            Container(
+              height: size.height *0.8,
+              width: size.width*0.75,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(63),
+                  bottomLeft: Radius.circular(63)
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 60,
+                    color: kPrimaryColor.withAlpha((255*0.29).round())
+                  )
+                ],
+                image: DecorationImage(
+                  alignment: Alignment.centerLeft,
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/img.png"))
+              ),
+            )
+          ],
+          ),
         )
       ],
     );
