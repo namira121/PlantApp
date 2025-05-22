3a4e33f09fbd54c4590cde56898513f253f2da6d
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-22 19:31:34 +0700

    menambahkan padding dan margin

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -16,15 +16,19 @@ class Body extends StatelessWidget {
             height: 24,
             child: Stack(
               children: <Widget>[
-                Text(
-                  "Recommended",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                Padding(
+                  padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+                  child: Text(
+                    "Recommended",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
                 ),
                 Positioned(
                   bottom: 0,
                   left: 0,
                   right: 0,
                   child: Container(
+                    margin: EdgeInsets.only(right: kDefaultPadding / 4),
                     height: 7,
                     color: kPrimaryColor.withAlpha((255*0.2).round()),
                   ))
