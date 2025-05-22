60aef0f7577138f2c4a82d6e46e92d401f000343
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-22 19:30:00 +0700

    menambahkan container dengan warna dari constants

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app_081/constants.dart';
 import 'headwithsearch.dart';
 
 class Body extends StatelessWidget {
@@ -18,7 +19,15 @@ class Body extends StatelessWidget {
                 Text(
                   "Recommended",
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                )
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    height: 7,
+                    color: kPrimaryColor.withAlpha((255*0.2).round()),
+                  ))
               ],
             ),
           )
