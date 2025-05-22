d843cb25a5df13ea635110e64964f78c4e1ea37b
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-23 03:04:28 +0700

    membuat widget untuk image dan icon

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -9,61 +9,80 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        SizedBox(
-          height: size.height*0.8,
-          child: Row(
-            children: <Widget>[
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: kDefaultPadding*3),
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: IconButton(
-                        padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                        onPressed: (){
-                          Navigator.pop(context);
-                        }, 
-                        icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
-                        ),
-                    ),
-                    Spacer(),
-                    IconCard(icon: "assets/icons/sun.svg",),
-                    IconCard(icon: "assets/icons/icon_2.svg",),
-                    IconCard(icon: "assets/icons/icon_3.svg",),
-                    IconCard(icon: "assets/icons/icon_4.svg",),
-                  ],
-                ),
-              )
-            ),
-            Container(
-              height: size.height *0.8,
-              width: size.width*0.75,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(63),
-                  bottomLeft: Radius.circular(63)
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 60,
-                    color: kPrimaryColor.withAlpha((255*0.29).round())
-                  )
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ImageAndIcon(size: size)
+        ],
+      ),
+    );
+  }
+}
+
+class ImageAndIcon extends StatelessWidget {
+  const ImageAndIcon({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: kDefaultPadding * 3),
+      child: SizedBox(
+        height: size.height*0.8,
+        child: Row(
+          children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: kDefaultPadding*3),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                      onPressed: (){
+                        Navigator.pop(context);
+                      }, 
+                      icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
+                      ),
+                  ),
+                  Spacer(),
+                  IconCard(icon: "assets/icons/sun.svg",),
+                  IconCard(icon: "assets/icons/icon_2.svg",),
+                  IconCard(icon: "assets/icons/icon_3.svg",),
+                  IconCard(icon: "assets/icons/icon_4.svg",),
                 ],
-                image: DecorationImage(
-                  alignment: Alignment.centerLeft,
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/images/img.png"))
               ),
             )
-          ],
           ),
-        )
-      ],
+          Container(
+            height: size.height *0.8,
+            width: size.width*0.75,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(63),
+                bottomLeft: Radius.circular(63)
+              ),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 10),
+                  blurRadius: 60,
+                  color: kPrimaryColor.withAlpha((255*0.29).round())
+                )
+              ],
+              image: DecorationImage(
+                alignment: Alignment.centerLeft,
+                fit: BoxFit.cover,
+                image: AssetImage("assets/images/img.png"))
+            ),
+          )
+        ],
+        ),
+      ),
     );
   }
 }
