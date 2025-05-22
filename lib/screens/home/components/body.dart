16ba050b2d08746690b8b532ffcbd88f6ed441c0
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-22 19:11:05 +0700

    memindahkan method headwithseach ke dalam file baru

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_app_081/constants.dart';
+import 'package:plant_app_081/screens/home/components/headwithsearch.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -15,88 +16,5 @@ class Body extends StatelessWidget {
         ],
       ),
     );
-  }
-
-  Container HeadWithSearch(Size size, BuildContext context) {
-    return Container(
-        margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
-        height: size.height * 0.2,
-        child: Stack(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(
-                left: kDefaultPadding,
-                right: kDefaultPadding,
-                bottom: 36 + kDefaultPadding,
-              ),
-              height: size.height * 0.2 - 27,
-              decoration: BoxDecoration(
-                color: kPrimaryColor,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(36),
-                  bottomRight: Radius.circular(36),
-                ),
-              ),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    'Hi Uishopy!',
-                    style: Theme.of(
-                      context,
-                    ).textTheme.headlineSmall!.copyWith(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Spacer(),
-                  Image.asset("assets/images/logo.png"),
-                ],
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                height: 54,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset(0, 10),
-                      blurRadius: 50,
-                      color: kPrimaryColor.withAlpha((255 * 0.5).round()),
-                    ),
-                  ],
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        onChanged: (value) {},
-                        decoration: InputDecoration(
-                          hintText: "Search",
-                          hintStyle: TextStyle(
-                            color: kPrimaryColor.withAlpha(
-                              (255 * 0.5).round(),
-                            ),
-                          ),
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    SvgPicture.asset("assets/icons/search.svg"),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
   }
 }
