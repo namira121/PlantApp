b0adb319f131f4844f7c01d82a8ccdc88e9c15a8
namira121 <127211008+namira121@users.noreply.github.com>
2025-05-23 02:41:05 +0700

    membuat iconbutton dengan icon yang sudah ada di assets

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:plant_app_081/constants.dart';
 
 class MyWidget extends StatelessWidget {
@@ -11,7 +12,17 @@ class MyWidget extends StatelessWidget {
       children: [
         Row(children: [
           Expanded(
-            child: Column()
+            child: Column(
+              children: [
+                IconButton(
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  onPressed: (){
+                    Navigator.pop(context);
+                  }, 
+                  icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
+                  )
+              ],
+            )
           ),
           Container(
             height: size.height *0.8,
